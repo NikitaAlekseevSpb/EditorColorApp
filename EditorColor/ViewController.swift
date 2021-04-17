0308b287249527f95f29fb99393cd1230bb56d16
Nik <nik.alekseev.spb@gmail.com>
2021-04-18 00:52:48 +0300

    creat new View for general screen

--- EditorColor/ViewController.swift
+++ EditorColor/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var viewColor: UIView!
+    
+    @IBOutlet var redLevel: UILabel!
+    @IBOutlet var greenLevel: UILabel!
+    @IBOutlet var blueLevel: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        viewColor.layer.cornerRadius = 10
+        
+        viewColor.backgroundColor = .black
+        
+        
+        sliderRed.minimumTrackTintColor = .red
+        sliderGreen.minimumTrackTintColor = .green
+        sliderBlue.minimumTrackTintColor = .blue
     }
 
-
+    @IBAction func actionSliderOfRed() {
+        redLevel.text = String(format: "%.2f", sliderRed.value)
+        
+        setColor()
+        
+    }
+    
+    @IBAction func actionSliderOfGreen() {
+        greenLevel.text = String(format: "%.2f", sliderGreen.value)
+        
+        setColor()
+    }
+    
+    @IBAction func actionSliderOfBlue() {
+        blueLevel.text = String(format: "%.2f", sliderBlue.value)
+        
+        setColor()
+    }
+    
+    private func setColor() {
+        viewColor.backgroundColor = .init(
+            red: CGFloat(sliderRed.value),
+            green: CGFloat(sliderGreen.value),
+            blue: CGFloat(sliderBlue.value),
+            alpha: 1)
+    }
 }
 
